d7a19a1a22a8537fbaf38d70550df380a20beb46
Suzanne <STian@lcs.on.ca>
2022-05-07 19:20:59 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Suzanne Tian on 2022-05-07.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
